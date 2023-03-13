3968f353e310bdbd48809b5097af0355b962f55e
adrianMartinezTeba <adrymate@gmail.com>
2023-03-13 17:15:00 +0100

    dd

--- main.js
+++ main.js
@@ -17,45 +17,16 @@ const mostrarPreguntas = () => {
 
 }
 // mostrarPreguntas()
+function verApi() {
+    axios.get()
+}
 const cogerPreguntas = async () => {
     try {
-        const response = await axios.get(API_URL);
-        const preguntas = response.data.results;
-        quizContainer.innerHTML = ''; 
-    
-    
-        const preguntasDesordenadas = Array.from(Array(preguntas.length).keys()).sort(() => Math.random() - 0.5);
-    
-        preguntasDesordenadas.forEach((i) => {
-          const preguntaText = preguntas[i].question;
-    
-          const preguntaElement = document.createElement('h2');
-          preguntaElement.textContent = preguntaText;
-    
-          const respuestas = preguntas[i].incorrect_answers.concat(preguntas[i].correct_answer);
-    
-          const respuestasDesordenadas = Array.from(Array(respuestas.length).keys()).sort(() => Math.random() - 0.5);
-    
-          const contenedorRespuestas = document.createElement('div');
-    
-          respuestasDesordenadas.forEach((j) => {
-            const respuestaText = respuestas[j];
-    
-            const respuestaButton = document.createElement('button');
-            respuestaButton.textContent = respuestaText;
-            respuestaButton.classList.add('btn');
-            respuestaButton.addEventListener('click', () => {
-            });
-    
-            contenedorRespuestas.appendChild(respuestaButton);
-          });
-    
-          quizContainer.appendChild(preguntaElement);
-          quizContainer.appendChild(contenedorRespuestas);
-        });
-      } catch (error) {
+     const leerAPI= await axios.get(API_URL) 
+       console.log(leerAPI.data);
+        
+    } catch (error) {
         console.error(error);
-      }
     }
-    
-    mostrarPreguntas();
+}
+cogerPreguntas()
