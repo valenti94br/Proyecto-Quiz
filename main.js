f07069c5b9825f7d9e6b8382fbf9141e24348fa3
valenti94br <valenti94br@gmail.com>
2023-03-14 11:50:58 +0100

    cosas

--- main.js
+++ main.js
@@ -1,42 +1,66 @@
-const homeContainer = document.querySelector('#homeContainer')
-const btnEmpezar = document.querySelector('#btnempezar')
-const btnSiguiente = document.querySelector('#btnsiguiente')
-const quizContainer = document.querySelector('#quizContainer')
-const resultadosContainer = document.querySelector('#resultadosContainer')
-const API_URL = 'https://opentdb.com/api.php?amount=10&category=31&difficulty=easy'
-const preguntaElemento = document.querySelector('#preguntaElemento')
-const contenedorRespuestas = document.querySelector('#answers-buttons')
-let currentQuestionIndex;
+const homeContainer = document.querySelector('#homeContainer');
+const btnHome = document.querySelector('#btnHome');
+const quizContainer = document.querySelector('#quizContainer');
+const resultadosContainer = document.querySelector('#resultadosContainer');
 
-const empezarQuiz = () => {
-    homeContainer.classList.add('hide')
-    currentQuestionIndex = 0;
-    quizContainer.classList.remove('hide')
-    // setNextQuestion()
+const empezarQuiz = (e) => {
+  e.preventDefault();
+  homeContainer.classList.add('hide');
+  quizContainer.classList.remove('hide');
+};
 
-}
-// btnHome.addEventListener('click', empezarQuiz)
-// quizContainer.innerHTML = '';
-const mostrarPreguntas = (preguntasDelObjeto) => {
-quizContainer.innerHTML=`<div>${preguntas.question}</div>`
-    preguntasDelObjeto.answers.forEach(answer => {
-        quizContainer.innerHTML += `<div>${pregunta.question}</div>`
+btnHome.addEventListener('click', empezarQuiz);
+
+const API_URL = 'https://opentdb.com/api.php?amount=10&category=31&difficulty=medium';
+const preguntaElemento = document.querySelector('#preguntaElemento');
+const contenedorRespuestas = document.querySelector('#answers-buttons');
+
+const mostrarPreguntas = (preguntas) => {
+
+  quizContainer.innerHTML = '';
+
+  // mostrarPreguntas()
+
+  preguntas.forEach((pregunta) => {
+
+    const preguntaElement = document.createElement('h2');
+    preguntaElement.textContent = pregunta.question;
+    
+
+    const contenedorRespuestas = document.createElement('div');
+    
+
+    const respuestasDesordenadas = pregunta.incorrect_answers.concat(pregunta.correct_answer).sort(() => Math.random() - 0.5);
+    
+
+    respuestasDesordenadas.forEach((respuesta) => {
+      const respuestaButton = document.createElement('button');
+      respuestaButton.textContent = respuesta;
+      respuestaButton.classList.add('btn');
+      respuestaButton.addEventListener('click', () => {});
+    
+      contenedorRespuestas.appendChild(respuestaButton);
     });
+    
 
-}
-// mostrarPreguntas()
+    quizContainer.appendChild(preguntaElement);
+    quizContainer.appendChild(contenedorRespuestas);
+  });
+};
+
 const cogerPreguntas = async () => {
-    try {
-        const response = await axios.get(API_URL)
-        const Preguntas = response.data.results
-        mostrarPreguntas(Preguntas)
-        console.log(Preguntas);
-    } catch (error) {
-        console.error(error);
-    }
-}
-cogerPreguntas()
-const siguientePregunta = () => {
-    // resetState();
-    mostrarPreguntas(Preguntas.question[currentQuestionIndex]);
-}
+  try {
+    const response = await axios.get(API_URL);
+    const preguntas = response.data.results;
+    
+
+    mostrarPreguntas(preguntas);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+cogerPreguntas();
+
+
+
