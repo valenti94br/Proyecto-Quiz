066f23a4e2c818c9c3a0a426b8b0f0ea39546f97
adrianMartinezTeba <adrymate@gmail.com>
2023-03-14 12:41:55 +0100

    preguntas en un array

--- main.js
+++ main.js
@@ -7,18 +7,23 @@ const API_URL = 'https://opentdb.com/api.php?amount=10&category=31&difficulty=ea
 const preguntaElemento = document.querySelector('#preguntaElemento')
 const contenedorPreguntas = document.querySelector('#preguntaContainer')
 let currentQuestionIndex;
-
-
+let preguntas = []
 const cogerPreguntasAPI = async () => {
     try {
         const response = await axios.get(API_URL)
         const listado = response.data.results
+        listado.forEach(objeto => {
+            preguntas.push(objeto.question)//array con solo preguntas
+            console.log(preguntas);
+        });
         console.log(listado);
     } catch (error) {
         console.error(error);
     }
 }
 cogerPreguntasAPI()
+
+
 const empezarQuiz = (e) => {
     e.preventDefault()
     homeContainer.classList.add('hide')
