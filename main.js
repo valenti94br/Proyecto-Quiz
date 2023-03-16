04f87b3e281553170672ac3a59a06992c0aed309
adrianMartinezTeba <adrymate@gmail.com>
2023-03-16 17:58:40 +0100

    2 parte hecha

--- main.js
+++ main.js
@@ -11,26 +11,102 @@ console.log(nextQuestionBtn);
 const API_URL = 'https://opentdb.com/api.php?amount=10&type=multiple'
 console.log(API_URL);
 
-let preguntas = [];
-let currentQuestion = 0;
+let preguntasObject = [];
+let currentQuestion=0 ;
 const cogerPreguntasApi = async () => {
     try {
         const response = await axios.get(API_URL)
-        preguntas = response.data.results
+        let preguntas = response.data.results
         console.log(preguntas);
         //todas las preguntas en un array de objetos hago un bucle para que me trabaje sobre cada una de las preguntas
         preguntas.forEach(pregunta => {
             const ask = pregunta.question
-            console.log(ask);
-            // guardo la pregunta en una variable para luego meterla en el html
-            const answers = [...pregunta.incorrect_answers,pregunta.correct_answer]
-            console.log(answers);
-            //lo mismo con las respuestas los 3 puntos significa que rompe su array para que asi esten todas en un unico arrays(incorrect_answers es un arrar con 3 mal y el otro es una key solo con una string por eso solo en el primero si no tambn en el otro para romper el array  tambn) las malas y buenas
-            //luego no se como habria que hacerlo para recorrer este array y que saliese todo en el html la cosa es que tampoco se como avanzar pq me ha llevado rato pillar lo de los puntos
-        });
+            // console.log(ask);
+            const respCorrecta = [{ text: pregunta.correct_answer, correct: true }]
+            // console.log(respCorrecta)
+
+            const respIncorrectas = []
+            pregunta.incorrect_answers.forEach(answer => {
+                let incans = { text: answer, correct: false }
+                respIncorrectas.push(incans)
+            });
+
+            // console.log(respIncorrectas);
+
+            const answers = [...respCorrecta, ...respIncorrectas]
+
+            answers.sort(() => Math.random() - Math.random());
+            //esto hace que salgan las respuestas desordenadas pero no entiendo como funciona
+            // console.log(answers);
+            const askAndAnswers = { pregunta: ask, respuestas: answers }
+            // console.log(askAndAnswers);
+            preguntasObject.push(askAndAnswers)
+            return preguntasObject
+        })
+        console.log(preguntasObject);
+        //este codigo hace que coja de api los datos y los voy adecuando de tal forma para que cuando se ejecute este funcion que sera al hacer click en empezar y se forma un array de objetos deonde cada objeto tiene una pregunta y sus respuestas donde cada respuesta es un objeto con dos propiedades una el texto y otra si es la correcta
     } catch (error) {
         console.error(error);
     }
 }
-cogerPreguntasApi()
-//te lo dejo hasta aqui asi y si tocas ya tu me dices pq no estoy seguro de que hasta aqui sea asi pero es lo mas aseado que me ha salido por consola y por aqui asi que ahora voy a desconectar no se cual seria el siguiente paso ahora mismo pero quizas sea tirar por meterlo en el html en el dom pero eso con el tema del SCOPE no se como va a salir pq estan todas las variables dentro de es afuncion y yo con eso me lio y tambn habria que tener en cuenta que para que saliese de una en una habia que hacer lo de currentQuestion e ir limpiando con cada click y salga una nueva pregunta ahora mismo esto ultimo que he dicho tomalo mas como corazonada pq no tengo ni idea de vd me meto con eso esta noche o mañana
+// cogerPreguntasApi()
+// console.log(preguntasObject);
+const empezarQuiz = async (e) => {
+    e.preventDefault();
+    startBtn.classList.add('hide')
+    nextQuestionBtn.classList.remove('hide')
+    questionContainer.classList.remove('hide')
+   await cogerPreguntasApi(); // Espera a que se resuelva la promesa
+    siguientePregunta(); // Muestra la primera pregunta
+}
+
+const mostrarPregunta = preguntasBloque => {
+    questionText.innerText = preguntasBloque.pregunta;
+    preguntasBloque.respuestas.forEach(respuesta => {
+        const btn = document.createElement('button')
+        btn.innerText = respuesta.text
+        if (respuesta.correct === true) {
+            btn.dataset.correct = true
+            localStorage.setItem
+        }
+        btn.addEventListener('click', seleccionarPregunta)
+        optionsContainer.appendChild(btn)
+    })
+}
+function setStatusClass(element) {
+    if (element.dataset.correct) {
+      element.classList.add("correct");
+    } else {
+        element.classList.add("wrong");
+    }
+}
+const siguientePregunta = () => {
+    resetState();
+    mostrarPregunta(preguntasObject[currentQuestion])
+}
+const seleccionarPregunta = () =>{
+    Array.from(optionsContainer.children).forEach(btn => {
+        setStatusClass(btn)
+    });
+    if (preguntasObject.length>currentQuestion + 1) {
+        
+        nextQuestionBtn.classList.remove('hide')
+
+    } else {
+        // startBtn.innerText = 'restart'
+        // startBtn.classList.remove('hide')
+        questionContainer.innerText = 'has acabado'
+    }
+}
+function resetState() {
+    nextQuestionBtn.classList.add("hide");
+    while (optionsContainer.firstChild) {
+        optionsContainer.removeChild(optionsContainer.firstChild);
+    }
+}
+startBtn.addEventListener('click', empezarQuiz);
+nextQuestionBtn.addEventListener('click',()=>{
+    currentQuestion++;
+    siguientePregunta()
+})
+
